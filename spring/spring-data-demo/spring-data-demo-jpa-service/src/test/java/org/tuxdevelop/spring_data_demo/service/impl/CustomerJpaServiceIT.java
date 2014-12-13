52f422a32f2763556c6bd4156277a6383d8f3e98
tuxdevelop <tuxdevelop@gmail.com>
2014-12-13 21:31:26 +0100

    added IT

---
+++ spring/spring-data-demo/spring-data-demo-jpa-service/src/test/java/org/tuxdevelop/spring_data_demo/service/impl/CustomerJpaServiceIT.java
@@ -0,0 +1,32 @@
+package org.tuxdevelop.spring_data_demo.service.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.tuxdevelop.spring_data_demo.configuration.PersistenceJPAConfiguration;
+import org.tuxdevelop.spring_data_demo.jpa.domain.Customer;
+import org.tuxdevelop.spring_data_demo.service.CustomerService;
+import org.tuxdevelop.spring_data_demo.util.CustomerFactory;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = PersistenceJPAConfiguration.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class CustomerJpaServiceIT {
+
+    @Autowired
+    private CustomerService customerJpaService;
+
+    @Test
+    public void addCustomerIT(){
+        final Customer customer = CustomerFactory.createCustomer();
+        final Customer addedCustomer = customerJpaService.addCustomer(customer);
+        Assert.assertNotNull(addedCustomer);
+    }
+
+
+
+}

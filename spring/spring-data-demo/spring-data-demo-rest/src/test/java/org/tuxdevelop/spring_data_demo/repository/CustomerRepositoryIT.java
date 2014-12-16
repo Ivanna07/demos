ac192e73cc4d6426cd29294b63313f6b2cf31bbd
tuxdevelop <tuxdevelop@gmail.com>
2014-12-16 14:37:23 +0100

    added IT

--- spring/spring-data-demo/spring-data-demo-rest/src/test/java/org/tuxdevelop/spring_data_demo/repository/CustomerRepositoryIT.java
+++ spring/spring-data-demo/spring-data-demo-rest/src/test/java/org/tuxdevelop/spring_data_demo/repository/CustomerRepositoryIT.java
@@ -1,6 +1,42 @@
 package org.tuxdevelop.spring_data_demo.repository;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.tuxdevelop.spring_data_demo.rest.SpringDataRestApplication;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringDataRestApplication.class)
+@WebAppConfiguration
 public class CustomerRepositoryIT {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void init(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+    }
+
+    @Test
+    public void getCustomerIT() throws Exception {
+        final MvcResult result =
+                mockMvc.perform(get("/customers/1")).andExpect(status().isOk()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+    }
 
 }

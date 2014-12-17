bec81ab753c0772c48df636c5b61daf7070885e7
tuxdevelop <tuxdevelop@gmail.com>
2014-12-17 09:40:55 +0100

    added new finder for contact

--- spring/spring-data-demo/spring-data-demo-rest/src/main/java/org/tuxdevelop/spring_data_demo/rest/repository/ContactRepository.java
+++ spring/spring-data-demo/spring-data-demo-rest/src/main/java/org/tuxdevelop/spring_data_demo/rest/repository/ContactRepository.java
@@ -1,10 +1,16 @@
 package org.tuxdevelop.spring_data_demo.rest.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.tuxdevelop.spring_data_demo.jpa.domain.Contact;
+import org.tuxdevelop.spring_data_demo.jpa.domain.ContactClassifier;
 
 //@RepositoryRestResource(path = "contacts",collectionResourceRel = "contacts")
 @RepositoryRestController
-public interface ContactRepository extends PagingAndSortingRepository<Contact,Long>{
+public interface ContactRepository extends PagingAndSortingRepository<Contact, Long> {
+
+    Contact findByCustomerIdAndContactClassifier(@Param("customerId") Long customerId, @Param("contactClassifier")
+    ContactClassifier contactClassifier);
+
 }

7f83764b6181ab36535f2369662e41e8e48796be
tuxdevelop <tuxdevelop@gmail.com>
2014-12-25 02:10:57 +0100

    refactored imports

--- spring/spring-boot-cxf-demo/src/main/java/org/tuxdevelop/spring_boot_cxf_demo/repository/ContactRepository.java
+++ spring/spring-boot-cxf-demo/src/main/java/org/tuxdevelop/spring_boot_cxf_demo/repository/ContactRepository.java
@@ -3,12 +3,11 @@ package org.tuxdevelop.spring_boot_cxf_demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import org.tuxdevelop.spring_boot_cxf_demo.domain.Contact;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
-    @Query(value = "SELECT c FROM Contact AS c WHERE c.contactClassifier = 'STANDARD' AND c.customer.id = :customerId")
-    public Contact getStandardContactOfCustomer(@Param(value = "customerId") final Long customerId);
+	@Query(value = "SELECT c FROM Contact AS c WHERE c.contactClassifier = 'STANDARD' AND c.customer.id = :customerId")
+	public Contact getStandardContactOfCustomer(@Param(value = "customerId") final Long customerId);
 
 }
